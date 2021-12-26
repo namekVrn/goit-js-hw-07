e702f56d2d4f38b3a93b9e27da86885a328a2cf6
Andrey <gsmer.prof@yandex.ru>
2021-12-26 18:13:53 +0300

    renew

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const boxgallery = document.querySelector('.gallery') // Нашли ссылку на объект
+const imgLink = document.querySelectorAll("a") // Cсылка на ссылку 
+console.log(imgLink)
+const getImg = ({original, preview, description}) => {
+  return `<a href="${original}" class="gallery__link"><img class="gallery__image" data-source="${original}" src="${preview}" alt="${description}"></a>`
+}                                                     // Создали разметку
+const resultHtml = galleryItems.map(getImg).join('')     // мапнули массив объектов с переданной сслкой на функцию
+boxgallery.insertAdjacentHTML("beforeend", resultHtml)
+console.log(boxgallery)
+
+boxgallery.addEventListener('click', onGallety)
+function onGallety(event){
+event.preventDefault()
+if(event.target.nodeName !== "IMG"){
+return
+}
+const instance = basicLightbox.create(`
+    <img src="${event.target.src}" width="800" height="600">
+`)
+
+instance.show()
+console.log(event.target)
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery')
+const renderHtml = ({original, preview,description })=>{
+    return ` <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      title="${description}"
+    />
+  </a>`
+}
+
+const resultHtml = galleryItems.map((renderHtml)).join("")
+const getRenderHtml = galleryBox.insertAdjacentHTML("beforeend",resultHtml)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 2500,
+    captionPosition: 'bottom',
+    captionClass: "center"
+
+});
