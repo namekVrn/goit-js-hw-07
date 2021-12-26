bd6bb723e7791e64b37219eb89f3b0778c466822
Andrey <gsmer.prof@yandex.ru>
2021-12-26 18:15:05 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -15,7 +15,7 @@ const resultHtml = galleryItems.map((renderHtml)).join("")
 const getRenderHtml = galleryBox.insertAdjacentHTML("beforeend",resultHtml)
 
 const lightbox = new SimpleLightbox('.gallery a', {
-    captionDelay: 2500,
+    captionDelay: 250,
     captionPosition: 'bottom',
     captionClass: "center"
 
